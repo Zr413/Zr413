6ce6d88869ead47f62b2f9cf1ee13cc06c0a8654
Zr413 <z0rin.13@yandex.ru>
2022-09-19 23:02:37 +0300

    add space

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from time import sleep
 
+
 driver = webdriver.Chrome('/media/zft/HDD1T/projects/Zr413/Zr413/chromedriver')
 driver.get('https://google.com')
 driver.find_element(By.XPATH, '//*[@id="input"]').send_keys('Skillfactory' + Keys.RETURN)
